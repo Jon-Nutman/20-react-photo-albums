63493f6f9e29d188f0bfa6baf168426ee3d3afc2
Jon-Nutman <jonnut730@gmail.com>
2021-02-12 17:55:04 -0500

    not working

--- albums/src/pages/Album.js
+++ albums/src/pages/Album.js
@@ -1,3 +1,26 @@
+import { useState, useEffect } from 'react'
+import {
+    BrowserRouter as Router,
+    Switch,
+    Route,
+    Link
+  } from 'react-router-dom'
+
+
 export default function Album(props) {
-    return <div>album</div>
+    const [albums, setAlbums] = useState([])
+useEffect(() =>{
+    const url = 'http://localhost:3001/albums'
+    fetch(url)
+    .then(response => response.json())
+    .then(data => {
+        setAlbums(data)
+        console.log(data)
+    })
+}, [])
+    return <div>
+        <ul>
+           <Link to={`/detail/${albums.id}`}>{albums.map(album => <li key={album.id}> <img  src={album.thumbnail} alt="Logo" /> {album.name} </li>)}</Link>
+        </ul>
+    </div>
 }
--- albums/src/pages/AlbumDetail.js
+++ albums/src/pages/AlbumDetail.js
@@ -1,3 +1,34 @@
+import { useState, useEffect } from 'react'
+import {
+    BrowserRouter as Router,
+    Switch,
+    Route,
+    Link
+  } from 'react-router-dom'
+
 export default function AlbumDetail(props) {
-    return <div>album detail</div>
+    const [activeAlbum, setActiveAlbum] = useState([])
+useEffect(() =>{
+    const url = 'http://localhost:3001/albums/:id'
+    fetch(url)
+    .then(response => response.json())
+    .then(data => {
+        console.log(data)
+        setActiveAlbum(data)
+        console.log(activeAlbum)
+    })
+   
+}, [])
+
+    return <div>
+
+    </div>
+
+
+
+//     <div>
+//     <ul>
+//         {activeAlbum.photos.map(album => <li> <img src={activeAlbum.photos.photos.thumbnail} alt="Logo" /> {activeAlbum.photos.photos.name} </li>)}
+//     </ul>
+// </div>
 }
