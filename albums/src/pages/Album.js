fd389d34e75d8ae90ef981dc1e8249e8dc7e6a73
Jon-Nutman <jonnut730@gmail.com>
2021-02-15 11:27:12 -0500

    url updates but page wont change reload but idk cause spa

--- albums/src/pages/Album.js
+++ albums/src/pages/Album.js
@@ -1,27 +1,37 @@
-import { useState, useEffect } from "react";
-import {
-    BrowserRouter as Router,
-    Switch,
-    Route,
-    Link
-  } from 'react-router-dom'
-
+import { useState, useEffect } from "react"
+import { BrowserRouter as Router, Switch, Route, Link } from "react-router-dom"
 
 export default function Album(props) {
-  const [albums, setAlbums] = useState([]);
+  const [albums, setAlbums] = useState([])
 
   useEffect(() => {
     fetch("http://localhost:3001/albums")
       .then((resp) => resp.json())
       .then((data) => {
-        console.log("data", data);
-        setAlbums(data);
-      });
-  }, []);
+        console.log("data", data)
+        setAlbums(data)
+      })
+  }, [])
   return (
-    <div>
-        <ul>
-           {albums.map(album => <li key={album.id}> <Link to={`/detail/${album.id}`}> <img  src={album.thumbnail} alt="Logo" /> {album.name}</Link> </li>)}
-        </ul>
+    <div className="main-container">
+      <h1 className="header">My Albums</h1>
+      <div className="album-list">
+        {albums.map((album) => (
+          <span className="album-card-container">
+            {" "}
+            <div className="album-card" key={album.id}>
+              {" "}
+              <Link to={`/detail/${album.id}`}>
+                {" "}
+                <div>
+                <span className='album-thumbnail'> <img src={album.thumbnail} alt="Logo" /> </span>
+                </div>{" "}
+                <span className='album-name'> {album.name}</span>
+              </Link>{" "}
+            </div>
+          </span>
+        ))}
+      </div>
     </div>
-  )}
+  )
+}
--- albums/src/pages/AlbumDetail.js
+++ albums/src/pages/AlbumDetail.js
@@ -1,14 +1,7 @@
-import { useState, useEffect } from "react";
-import {
-    BrowserRouter as Router,
-    Switch,
-    Route,
-    Link
-  } from 'react-router-dom'
+import { useState, useEffect } from "react"
+import { BrowserRouter as Router, Switch, Route, Link } from "react-router-dom"
 
-
-  export default function AlbumDetail(props) {
-   
+export default function AlbumDetail(props) {
   const [activeAlbum, setActiveAlbum] = useState({})
   console.log(activeAlbum)
   useEffect(() => {
@@ -19,11 +12,33 @@ import {
         setActiveAlbum(data)
       })
   }, [])
-  return (
-    <div>
-        <ul>
+  const [albums, setAlbums] = useState([])
+  useEffect(() => {
+    fetch("http://localhost:3001/albums")
+      .then((resp) => resp.json())
+      .then((data) => {
+        console.log("data", data)
+        setAlbums(data)
+      })
+  }, [])
 
-           {activeAlbum.photos ? activeAlbum.photos.map(album => <li key={album.id}> <img  src={album.thumbnail} alt="Logo" /> {album.name} </li>): null}
-        </ul>
+  return (
+    <div className='album-detail-container'><ul className='album-nav-side'>
+      {albums.map((album) => (<div>
+        <li className="album-detail-side-list-item"><Link  to={`/detail/${album.id}`} ><span>{album.name}</span></Link></li>
+      </div>
+
+      ))}  </ul>
+      <ul>
+        {activeAlbum.photos
+          ? activeAlbum.photos.map((photo) => (
+              <li key={photo.id}>
+                {" "}
+                <img src={photo.thumbnail} alt="Logo" /> {photo.name}{" "}
+              </li>
+            ))
+          : null}
+      </ul>
     </div>
-  )}
+  )
+}
